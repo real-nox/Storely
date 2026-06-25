33d077f4458506bb8dca9d1a9bb33de4b0f600cb
Rayane <sirri.rayane.ma@gmail.com>
2026-06-25 21:36:25 +0100

    Offers section

--- frontend/src/components/Offres.tsx
+++ frontend/src/components/Offres.tsx
@@ -1,7 +1,7 @@
 export default function Offres() {
     return (
         <div className="OffresContainer">
-            
+            <div className="BlankOffer border"></div>
         </div>
     )
 }
